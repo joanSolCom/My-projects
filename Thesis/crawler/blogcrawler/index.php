b688eb0ab01f4d141d5697ec93c6fe2f81e467e1
Juan <namek@namek.(none)>
2012-12-23 17:28:19 +0100

    Next Version

--- Thesis/crawler/blogcrawler/index.php
+++ Thesis/crawler/blogcrawler/index.php
@@ -3,24 +3,36 @@
 	require_once "./simplehtmldom/simple_html_dom.php";
 
 	//http://opinionator.blogs.nytimes.com/
-
-	$html = file_get_html('http://opinionator.blogs.nytimes.com');
+	$baseUrl = 'http://opinionator.blogs.nytimes.com/page/';
+	$page = 1;
+	$html = file_get_html($baseUrl.$page);
+	
 
 	$db = new PDO('mysql:host=localhost;dbname=blogcrawler', 'root', '1234');
-	//$stmt = $db->prepare("select contenttype, imagedata from images where id=?");
 	
+	$aInfoPosts = array();
+	$i = 0;
+
 	$elements = $html->find('.post');
 	foreach($elements as $element){
 
 		//Extract Title of the article
 		foreach($element->find('.entry-title') as $title){
-			echo $title->plaintext . "<br/>";
+			$aInfoPosts[$i]['title'] = $title->plaintext;
 		}
 
 		//Extract Author of the article
 		foreach($element->find("address a") as $author){
-			echo $author->plaintext . "<br/>";
+			$aInfoPosts[$i]['author'] = $author->plaintext;
 		}
+
+		//Extract the links to the full texts
+		foreach($element->find('.entry-title a') as $title){
+			$aInfoPosts[$i]['link'] = $title->href;
+		}
+		$i++;
 	}
+
+	var_dump($aInfoPosts);
        
 ?>
